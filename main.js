98dc1b2498a60bff39a69a5a59ac74a181bea1d4
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-13 15:00:12 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// var 
+
+var a = 45;
+
+for (var i = 0; i < 3; i++) {
+  // console.log(i);
+}
+
+console.log(i);
+console.log(a);
+
+// let 
+let value = 35;
+for (let j = 0; j < 3; j++) {
+  console.log(j);
+  console.log(value);
+}
+// console.log(j);
+
+// const 
+const PI = Math.PI;
+PI = 5464456;
+console.log(PI);
