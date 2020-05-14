a4c65ccc997af85e95682dba40f786e810b823aa
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-14 13:15:55 +0545

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,13 @@
 // d: switch
 
 
-// a: if
+// a: if else
 
 const age = 34;
 
 if (age >= 18) {
   console.log("You are eligible for voting");
+}
+else {
+  console.log("You are not eligible for voting");
 }
