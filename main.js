81e62012d1a2bb5b4c34991988ddbe469544c8db
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-14 13:14:01 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//conditional statement
+
+// a: if 
+// b: if else 
+// c: if else if
+// d: switch
+
+
+// a: if
+
+const age = 34;
+
+if (age >= 18) {
+  console.log("You are eligible for voting");
+}
