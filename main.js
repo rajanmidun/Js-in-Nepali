337316afea27ed1951e7d2cb08b596a66446e18e
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-14 12:47:25 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+//string
+
+let name = "Rajan Midun Magar Magar";
+let address = "Dhungeadda, Kathmandu";
+
+//length
+console.log(name.length);
+console.log(address.length);
+
+//postion of any word
+console.log(name.indexOf("Magar"));
+
+//position of last occurence of specific keyword/text
+console.log(name.lastIndexOf('Magar'));
+
+
+//concatinate two stirng
+// console.log(name.concat(address));
+console.log(name + ' ' + address);
+
+//type of name
+console.log(typeof name);
+
+
+//number
+const firstNumber = 45.56;
+console.log(firstNumber);
+console.log(typeof firstNumber);
+
+
+// boolean
+const check = true;
+console.log(check);
+console.log(typeof check);
+
+//undefined
+let hello;
+
+console.log(hello);
+console.log(typeof sfsfd);
+
+
+//null
+let name = null;
+let object1 = null;
+console.log(typeof name);
+
+
+//symbol
+const roll1 = Symbol();
+const roll2 = Symbol();
+
+console.log(roll1 === roll2);
