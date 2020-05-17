08dc88b6b1e06f0ed6ea0709670c195eaa222a58
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-17 14:36:37 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
-//do while loop
+//Arithmetic operator
 
-let i = 1;
+// Addition +
+//Subtraction -
+//Multiplication *
+//Division /
+//Remainder %
+// Exponentiation **
+//Increament ++
+//Decrement --
 
-do {
-  console.log("12 * " + i + " = " + (12 * i));  //12*1=12
-  i++;
-} while (i <= 10);
+let a = 6;
+let b = 5;
+
+console.log("Addition " + (a + b));
+console.log("Subtraction " + (a - b));
+console.log("Multiplication " + (a * b));
+console.log("Division " + (a / b));
+console.log("Remainder " + (a % b));
+console.log("Exponentiation " + (a ** b));// a^b 6^5;
+
+//Increment ++
+// let c = a++; //post increment => c=6 a=7
+// let c = ++a //pre increment =>c=7 a=7  
+// console.log(a, c);
+
+
+//Increment --
+// let c = a--; //post increment => c=6 a=5
+let c = --a //pre increment =>c=5 a=5  
+console.log(a, c);
+
+
+
+
+//Assignment operator
+let y = 5;
+let z = y; //z=5;
+
+// y += 5;// y=y+5=10
+// y -= 5;// y=y-5=0
+// y *= 5;// y=y*5=25
+// y /= 5;// y=y/5=1
+// y %= 5;// y=y%5=0
+y **= 5;// y=y^5=3125
+console.log(y);
