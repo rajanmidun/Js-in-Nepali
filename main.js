253b0a54d396405a7cd936ed4de7e872288a96fe
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-17 15:08:58 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,15 @@
-//Comparision operator
-// == equal to
-// === equal value and equal type
-// != not equal 
-// !== not equal value and not equal type
-// > greater than 
-// < less than 
-// greater than or equal to
-// <= less than or equal to
+//Ternary operator
 
-let first = "12";
-let second = 12;
+//syntax   => condition ? exprIfTrue : exprIfFalse
 
-// if (first === second) {
-//   console.log("yes");
+let age = 21;
+// if (age > 20) {
+//   console.log("Beer");
 // }
 // else {
-//   console.log("No");
+//   console.log("Juice");
 // }
 
-// if (first !== second) {  //12!==12
-//   console.log("yes");
-// }
-// else {
-//   console.log("No");
-// }
-
-if (first <= second) {
-  console.log("yes");
-}
-else {
-  console.log("No");
-}
-
-
-//Logical operator
-
-// && logical and  //all condition must be true
-// || logical or   //one or more condtion must be true
-// ! logical not
-
-
-let a = 12;
-let b = 9;
-
-if (!(a % 3 === 1)) { //false=true
-  console.log("Yes it is divisible by 3");
-}
-else {
-  console.log("No");
-}
-
+let result = (age > 20) ? "Beer" : "Juice";
+console.log(result);
 
