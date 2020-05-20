f3128e521f7f04eece404095512ff69d7b78ddac
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-20 10:31:06 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-//Ternary operator
+//break and continue
 
-//syntax   => condition ? exprIfTrue : exprIfFalse
+//break terminates the loop  and continues executing the code after the loop (if any):
+for(let i=1;i<=20;i++){
+  if(i==10)
+    break;
+  console.log("Number"+i);
+}
 
-let age = 21;
-// if (age > 20) {
-//   console.log("Beer");
-// }
-// else {
-//   console.log("Juice");
-// }
+console.log("End of program");
 
-let result = (age > 20) ? "Beer" : "Juice";
-console.log(result);
 
+/*continue doesnot terminate the loop but breaks one iteration (in the loop), 
+if a specified condition occurs, and continues with the next iteration in the loop.
+*/
+for(let i=1;i<=20;i++){
+  if(i==10)
+    continue;
+  console.log("Number"+i);
+}
+
+console.log("End of program");
