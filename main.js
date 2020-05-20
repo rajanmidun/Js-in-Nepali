079e78c89311c4d019a1bcfb4612b029d4bd9bb6
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-20 10:33:07 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
-//break and continue
+//function
 
-//break terminates the loop  and continues executing the code after the loop (if any):
-for(let i=1;i<=20;i++){
-  if(i==10)
-    break;
-  console.log("Number"+i);
+function message(){
+console.log("Hello from function")
 }
 
-console.log("End of program");
-
-
-/*continue doesnot terminate the loop but breaks one iteration (in the loop), 
-if a specified condition occurs, and continues with the next iteration in the loop.
-*/
-for(let i=1;i<=20;i++){
-  if(i==10)
-    continue;
-  console.log("Number"+i);
-}
-
-console.log("End of program");
+message();
+message();
+message();
