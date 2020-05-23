bb631609e2df55fec1969210237b46a72a615e60
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-23 16:37:54 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-//filter in js
-const rollNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+//map in js
+const salaries = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-// it creates new array
-const newRollNumbers = rollNumbers.filter(currentValue => currentValue >= 9);
+const newSalaries = salaries.reduce(salary => salary + 5);
 
-console.log(`Old array ${rollNumbers}`)
-console.log(`Old array ${newRollNumbers}`)
+console.log(`Old array ${salaries}`)
+console.log(`New array ${newSalaries}`)
