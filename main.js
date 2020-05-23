522f77438e39ebc8e016c9fb1eb57ba9d2f49019
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-23 16:36:58 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-//forEach in js
+//filter in js
 const rollNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-// for (let i = 0; i < rollNumbers.length; i++) {  //length=20
-//   console.log(rollNumbers[i]);
-// }
+// it creates new array
+const newRollNumbers = rollNumbers.filter(currentValue => currentValue >= 9);
 
-//foreach
-rollNumbers.forEach(function (rollNumber) {
-  console.log(rollNumber);
-})
+console.log(`Old array ${rollNumbers}`)
+console.log(`Old array ${newRollNumbers}`)
