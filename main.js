fa02646af176702aa0af56cb205c5d308b695c7b
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-28 18:53:24 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-// object inside array
+// Math object
 
-// const numbers = [1, 2, 3, 4, 5, 6, 5, 7];
 
-var user = {
-  firstname: "Raghav",
-  lastName: "Shrestha",
-  address: "Kathmandu"
-}
+console.log(Math);
 
-var users = [
-  { firstname: "Raghav", lastName: "Shrestha" },
-  { firstname: "Suraj", lastName: "Lohani" }
-]
+//Value of Pi
+console.log(Math.PI);
+console.log(Math.PI * 34 * 34);
 
-users.forEach(user => {
-  console.log(user.firstname);
-})
+//round off
+console.log(Math.round(7.8));
+
+//floor
+console.log(Math.floor(4.9));
+
+//ceil
+console.log(Math.ceil(4.1));
+
+// pow
+console.log(Math.pow(2, 3))// 2^3 
+
+
+//min max
+console.log(Math.min(13, 23, 33, 43, 53, 63, 7));
+console.log(Math.max(13, 23, 33, 43, 53, 63, 7));
+
+//sqrt
+console.log(Math.sqrt(64));
+
+//abs
+console.log(Math.abs(-1234));
+
+//Random
+// return value between 0 and 1
+console.log(Math.floor(Math.random() * 100)); //return numbers between 0 and 99
