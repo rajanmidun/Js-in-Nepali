9e8f4bad826d489c88da2bc2ab533579b1f0bc45
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-29 14:54:40 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,4 @@
-// Math object
+// document object model vs window object
 
-
-console.log(Math);
-
-//Value of Pi
-console.log(Math.PI);
-console.log(Math.PI * 34 * 34);
-
-//round off
-console.log(Math.round(7.8));
-
-//floor
-console.log(Math.floor(4.9));
-
-//ceil
-console.log(Math.ceil(4.1));
-
-// pow
-console.log(Math.pow(2, 3))// 2^3 
-
-
-//min max
-console.log(Math.min(13, 23, 33, 43, 53, 63, 7));
-console.log(Math.max(13, 23, 33, 43, 53, 63, 7));
-
-//sqrt
-console.log(Math.sqrt(64));
-
-//abs
-console.log(Math.abs(-1234));
-
-//Random
-// return value between 0 and 1
-console.log(Math.floor(Math.random() * 100)); //return numbers between 0 and 99
+console.log(window);
+console.log(document);
