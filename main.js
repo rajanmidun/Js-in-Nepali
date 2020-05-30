ed1a636907be366781b44d895f36219c05450223
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-30 17:43:27 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-//to select first element that contains ".section" class name
-//querySelector can take class,id,element etc as its parameter;
-const div = document.querySelector('.section');
-
-//to select all the elements start with p tag 
 const paragraphs = document.querySelectorAll('p');
 
-//to add the new html element
-//it returns only one element
-const byId = document.getElementById('message');
+paragraphs.forEach(paragraph => {
+  paragraph.innerText += " hello";
+})
 
-//to select the element using class
-//it returns all the elements that contain "section" class 
-const byClass = document.getElementsByClassName('section');
 
-//to select the element using class
-//it returns all the elements that contain p tag 
-const byClass = document.getElementsByTagName('p');
+const div = document.querySelector('.section');
+div.innerHTML += "<h1>Heading 1</h1>";
+
+const users = ["Rajan", 'Suraj', 'Raghav'];
+
+const ul = document.createElement('ul');
+users.forEach(user => {
+  const li = document.createElement('li');
+  li.innerText = user;
+  ul.appendChild(li);
+})
+
+div.appendChild(ul);
