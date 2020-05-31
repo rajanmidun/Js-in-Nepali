7829892b6dc1074488a878bc0fb250ea6fbf52ce
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-31 15:21:31 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-//to get the attribute
-const link = document.querySelector('a');
-console.log(link.getAttribute('href'));
+// //to get the attribute
+// const paragraph = document.querySelector('p');
+// console.log(paragraph.classList);
+
+// //remove the old value 
+// paragraph.classList.remove('primary');
+
+// //set the class attribute
+// // paragraph.classList.add('secondary');
+// paragraph.classList.toggle('secondary');
+// paragraph.classList.toggle('secondary');
 
 
-//set the attribut value
-link.setAttribute('href', 'https://www.facebook.com');
-link.innerText = "Facebook";
-
-//get the style properties
-console.log(link.style);
-
-//set the style properties
-link.style.color = "skyblue";
-link.style.fontSize = "12px";
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
+paragraphs.forEach(paragraph => {
+  // paragraph.innerHTML = "<h1>Heading</h1>";
+  if (paragraph.innerHTML.length > 10) {
+    paragraph.classList.add('primary');
+  }
+  else {
+    paragraph.classList.add('secondary');
+  }
+})
