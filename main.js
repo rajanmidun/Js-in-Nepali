31fbadb987f96da46f53d9ed86c6c08eb236bdf3
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-05-31 16:45:40 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//mouse events
+// click
+// mouseenter mouseover
+// mouseleave  mouseout
+// mousemove
+// mousedown
+
+const link = document.querySelector('a');
+
+// link.addEventListener('mousedown', () => {
+//   alert("You clicked the link");
+// })
+
+// link.onmousedown = function () {
+//   alert("You clicked the link");
+// }
