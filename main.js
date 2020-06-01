fb3ba576c3ba13b7144c46444b0fa85ad4dc1421
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-06-01 15:18:34 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//associative array
+//Arrays with named indexes
+
+// let user = [];
+// user[0] = 'Rajan';
+// user[1] = 'Magar';
+// user[2] = 'kathmandu';
+// user[4] = '345';
+// user[5] = 'male';
+// //to display the array;
+// document.write(user + '<br> <br>');
+// document.write(user[0] + "<br><br>");
+
+// document.write("Length is ", user.length);
+
+
+let user = [];
+user['firstname'] = 'Rajan';
+
+// document.write(user['firstname']);
+document.write(user.length);
