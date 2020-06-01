0dd1fc55ecc92f3b9019c8864abbc27bf1a2b610
Rajan Midun Magar <43414436+rajanmidun@users.noreply.github.com>
2020-06-01 20:05:58 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-//Form Validation
-//Examples
-// starting with abc /^abc/
-//ending with abc /abc$/
-//starting and ending with gef /^gef$/
-//should contain only letter and numbers /[a-zA-Z0-9]/
-//for checking any character /./
-//for checking length /.{6,10}/
-//for cheking min length 6 /.{6,}/
+//Accessing form in js
+//First way
+//const form =document.querySelector('form');
+//console.log(form);
+//console.log(form.username);
+//console.log(form.username1);
 
-
-const form = document.querySelector('form');
-const message = document.getElementById('message');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const username = form.username.value;
-
-  const pattern = /[a-zA-Z]{6,10}/;
-  const result = pattern.test(username);
-  if (!result) {
-    message.innerText = "Must be string and length must  be greater than 6 and less than 10";
-  }
-})
+//Second way
+const loginForm=document.forms[0];
+console.log(loginForm.length);
+console.log(loginForm.elements[1].name);
